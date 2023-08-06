159edd61a1a968622e23e043b0260306d1549a8a
luck22y <luckykumar2204@gmail.com>
2023-08-06 11:10:53 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import Button from './components/Button'
 import Tooltip from './components/Tooltip'
+import './App.css'
 
 const App = () => {
   return (
     <div className="container">
       <div style={{ padding: '50px' }}>
         <Tooltip position="top" text="This is a tooltip">
-          <Button text="Hover me" />
+          <Button text="Hover Me" />
         </Tooltip>&nbsp;&nbsp;
         <br /><br /><br />
         <Tooltip position="bottom" text="This is another tooltip">
-          <Button text="Hover again" />
+          <Button text="Hover Again" />
         </Tooltip>&nbsp;&nbsp;
         <br /><br /><br />
         <Tooltip position="left" text="This is a tooltip on the left">
-          <Button text="Hover left" />
+          <Button text="Hover Left" />
         </Tooltip>&nbsp;&nbsp;
         <br /><br /><br />
         <Tooltip position="right" text="This is a tooltip on the right">
-          <Button text="Hover right" />
+          <Button text="Hover Right" />
         </Tooltip>
       </div>
     </div>
@@ -27,6 +28,7 @@ const App = () => {
 };
 
 export default App;
+
 
 
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ text }) => {
   return (
-    <button style={{ padding: '10px', backgroundColor: '#f2c34e', border: 'none' }}>
+    <button style={{ padding: '10px', backgroundColor: 'GOLD', border: 'none' }}>
       {text}
     </button>
   );
